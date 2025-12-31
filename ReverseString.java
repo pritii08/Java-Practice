1bf85f5953eaf3ebc8aa8fb035127fa7af0fe67e
Preetyyy <pritimule08@gmail.com>
2025-12-31 14:35:54 +0530

    First question: Reversing a string

---
+++ ReverseString.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ReverseString{
+
+    public String reverseString(String s){
+        char[] arr=s.toCharArray();
+        StringBuilder str=new StringBuilder();
+
+        for(int i=arr.length-1;i>=0;i--){
+            str.append(arr[i]);
+        }
+
+        return str.toString();
+    }
+
+
+
+    public static void main(String arg[]){
+
+        Scanner sc=new Scanner(System.in);
+        ReverseString r=new ReverseString();
+
+        System.out.println("Enter the String : ");
+        String s=sc.nextLine();
+
+        System.out.println(s);
+        System.out.println(r.reverseString(s));
+
+
+
+    }
+}
