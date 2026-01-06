423aaebf562fab3d1290719712ccda8d27a3bff8
Preetyyy <pritimule08@gmail.com>
2026-01-06 17:12:56 +0530

    Sorting array (brute - force comparison sort)

--- array/SortArray.java
+++ array/SortArray.java
@@ -2,27 +2,10 @@ import java.util.*;
 
 public class SortArray{
 
-    public static void sortArrayAsce(int[] arr){
-        int n=arr.length;
-        //Ascending order
+    public static void sortArray(int[] arr){
+
         for(int i=0;i<arr.length;i++){
-            for(int j=0;j<n-i-1;j++){
-                if(arr[i]<arr[j]){
-                    int temp=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
-
-                }
-            }
-        }
-
-    }
-
-    public static void sortArrayDesc(int[] arr){
-        int n=arr.length;
-        //Desending order
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<n-i-1;j++){
+            for(int j=0;j<arr.length;j++){
                 if(arr[i]>arr[j]){
                     int temp=arr[i];
                     arr[i]=arr[j];
@@ -37,33 +20,11 @@ public class SortArray{
 
     public static void main(String arg[]){
 
-        Scanner sc=new Scanner(System.in);
-
-        System.out.println("Enter the size of array : ");
-        int n=sc.nextInt();
-
-        int[] arr=new int[n];
-
-        System.out.println("Enter the numbers : ");
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
+        int arr[]={10,80,30,79,99,1};
+        sortArray(arr);
+        for (int i : arr) {
+            System.out.println(i);
         }
-
-        System.out.println("Sorting array ascending : ");
-        sortArrayAsce(arr);
-        for(int i: arr){
-            System.out.print(i+" ");
-        }
-
-        System.out.println();
-
-        System.out.println("Sorting array descending : ");
-        sortArrayDesc(arr);
-        for(int i: arr){
-            System.out.print(i+" ");
-        }
-        
-        
 
     }
 }
