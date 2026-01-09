96d639d76eabf9db15110c76248c2f5cadbf3766
Preetyyy <pritimule08@gmail.com>
2026-01-09 14:34:51 +0530

    Insertion sort algorithm problem

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -1,3 +1,26 @@
+/***
+Insertion sort algorithm :
+We select an element and find its exact position and insert that element to its exact position
+
+dividing it into two part : Sorted | Unsorted
+
+Logic:
+
+for(int i=0 to n(array length)){
+    currentEle=arr[i]
+    int j=i-1;
+    while(j>=0 && current<arr[j]){
+        arr[j+1]=arr[j]
+        j--;
+    }
+    arr[j+1]=current
+}
+ */
+
+
+
+
+
 public class InsertionSort{
 
     public static void sortArray(int[] arr){
